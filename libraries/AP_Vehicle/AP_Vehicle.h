4eb9e315bb02a31577a70c402e6f366ad55e2997
Peter Barker <pbarker@barker.dropbear.id.au>
2021-08-22 13:10:56 +1000

    AP_Vehicle: add AUX_FUNCTION::POWEROFF

--- libraries/AP_Vehicle/AP_Vehicle.h
+++ libraries/AP_Vehicle/AP_Vehicle.h
@@ -241,6 +241,11 @@ public:
     // and flashing LEDs as appropriate
     void reboot(bool hold_in_bootloader);
 
+#if HAL_POWEROFF_ENABLED
+    void poweroff_start();
+    void poweroff();
+#endif
+
     /*
       get the distance to next wp in meters
       return false if failed or n/a
@@ -391,6 +396,13 @@ private:
     bool done_safety_init;
 
     uint32_t _last_internal_errors;  // backup of AP_InternalError::internal_errors bitmask
+
+#if HAL_POWEROFF_ENABLED
+    void poweroff_update();
+#if HAL_POWEROFF_DELAY_MS
+    uint32_t _poweroff_start_ms;
+#endif
+#endif
 };
 
 namespace AP {
