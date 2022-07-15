1294ca9924a9e211e160b8d3d6189144571ff0e4
Peter Barker <pbarker@barker.dropbear.id.au>
2022-07-15 12:38:38 +1000

    DEBUGDEBUG: add test script for smallest build size

---
+++ test.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+EXTRA_HWDEF="test-extra.hwdef"
+BOARD=MatekF405
+
+cat >"$EXTRA_HWDEF" <<EOF
+define GPS_BACKEND_DEFAULT_ENABLED 0
+define GPS_BACKEND_UBLOX_ENABLED 1
+
+undef HAL_PROBE_EXTERNAL_I2C_BAROS
+define HAL_PROBE_EXTERNAL_I2C_BAROS 0
+
+undef AP_BARO_BMP085_ENABLED
+define AP_BARO_BMP085_ENABLED 1
+undef AP_BARO_BMP280_ENABLED
+define AP_BARO_BMP280_ENABLED 1
+
+undef AP_RCPROTOCOL_BACKEND_DEFAULT_ENABLED
+define AP_RCPROTOCOL_BACKEND_DEFAULT_ENABLED 0
+define AP_RCPROTOCOL_SBUS_ENABLED 1
+
+define GPS_MAX_RECEIVERS 1
+
+define HAL_BUTTON_ENABLED 0
+
+define AP_RPM_ENABLED 0
+define AP_ICENGINE_ENABLED 0
+
+BOOTLOADER_EMBED 0
+
+define AP_ADVANCEDFAILSAFE_ENABLED 0
+
+EOF
+
+./Tools/autotest/test_build_options.py \
+    --no-run-with-defaults \
+    --no-disable-none \
+    --no-disable-in-turn \
+    --board="$BOARD" \
+    --extra-hwdef="$EXTRA_HWDEF"
