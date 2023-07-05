3a4906bd6a1d2d5dbe91a38b242f4dd6be6462e8
Peter Barker <pbarker@barker.dropbear.id.au>
2023-07-05 11:46:28 +1000

    SITL: improve Hirth simulation

--- libraries/SITL/SIM_EFI_Hirth.cpp
+++ libraries/SITL/SIM_EFI_Hirth.cpp
@@ -116,6 +116,47 @@ void EFI_Hirth::update_send()
     }
 }
 
+void EFI_Hirth::update_engine_model()
+{
+    auto sitl = AP::sitl();
+
+    // FIXME: this should come from simulation, not baro.  baro gets
+    // warmed by the simulated electronics!
+    const float ambient = AP::baro().get_temperature();
+
+    const uint32_t now_ms = AP_HAL::millis();
+
+    const float delta_t = (now_ms - engine.last_update_ms) * 1e-6;
+    engine.last_update_ms = now_ms;
+
+    // lose heat to environment (air-cooling due to airspeed and prop
+    // airflow could be taken into account here)
+    const float ENV_LOSS_FACTOR = 25;
+    engine.cht1_temperature -= (engine.cht1_temperature - ambient) * delta_t * ENV_LOSS_FACTOR;
+    engine.cht2_temperature -= (engine.cht2_temperature - ambient) * delta_t * ENV_LOSS_FACTOR;
+
+    const float rpm = sitl->state.rpm[0];
+    const float RPM_GAIN_FACTOR_CHT1 = 10;
+    const float RPM_GAIN_FACTOR_CHT2 = 8;
+    engine.cht1_temperature += rpm * delta_t * RPM_GAIN_FACTOR_CHT1;
+    engine.cht2_temperature += rpm * delta_t * RPM_GAIN_FACTOR_CHT2;
+}
+
+void EFI_Hirth::init()
+{
+    // auto sitl = AP::sitl();
+
+    if (is_zero(AP::baro().get_temperature())) {
+        // defer until the baro has had a chance to update....
+        return;
+    }
+
+    engine.cht1_temperature = AP::baro().get_temperature();
+    engine.cht2_temperature = AP::baro().get_temperature();
+
+    init_done = true;
+}
+
 void EFI_Hirth::update()
 {
     auto sitl = AP::sitl();
@@ -123,44 +164,82 @@ void EFI_Hirth::update()
         return;
     }
 
+    if (!init_done) {
+        init();
+    }
+
     // update throttle; interim thing to make life a little more interesting
-    // gcs().send_text(MAV_SEVERITY_INFO, "using throttle %f", settings.throttle/10.0);
-    throttle = 0.9 * throttle + 0.1 * (settings.throttle)/10.0;
+    throttle = 0.9 * throttle + 0.1 * settings.throttle/10;
+
+    update_engine_model();
 
     update_receive();
     update_send();
+}
+
+uint16_t EFI_Hirth::engine_status_field_value() const
+{
+    return (
+        0U << 0 |  // engine temperature sensor
+        1U << 1 |  // air temperature sensor
+        1U << 2 |  // air pressure sensor
+        1U << 3    // throttle sensor OK
+        );
 }
 
 void SITL::EFI_Hirth::send_record1()
 {
     const auto *sitl = AP::sitl();
 
+    // notionally the field updates should happen in the update()
+    // method, but here to save CPU for now:
     auto &r = packed_record1.record;
-
+    r.engine_status = engine_status_field_value();
     r.rpm = sitl->state.rpm[0];
-    gcs().send_text(MAV_SEVERITY_INFO, "rpm in: %u", r.rpm);
     r.air_temperature = AP::baro().get_temperature();
+    r.throttle = settings.throttle / 10;  // just echo this back
 
     packed_record1.update_checksum();
 
-    static_assert(sizeof(record1) == 84, "correct number of bytes in record1");
     write_to_autopilot((char*)&packed_record1, sizeof(packed_record1));
+
+    assert_storage_size<Record1, 84> _assert_storage_size_Record1;
+    (void)_assert_storage_size_Record1;
 }
 
 void SITL::EFI_Hirth::send_record2()
 {
-    packed_record2.record.throttle = uint16_t(throttle * 10.0);
+    const auto *sitl = AP::sitl();
+
+    // notionally the field updates should happen in the update()
+    // method, but here to save CPU for now:
+    auto &r = packed_record2.record;
+    r.throttle_percent_times_10 = throttle * 10.0;
+    r.fuel_consumption = ((MAX(sitl->state.rpm[0] - 1500.0, 0)) /2200.0) * 10;  // from log, very rough
+
     packed_record2.update_checksum();
 
-    static_assert(sizeof(record2) == 98, "correct number of bytes in record2");
     write_to_autopilot((char*)&packed_record2, sizeof(packed_record2));
+
+    assert_storage_size<Record2, 98> _assert_storage_size_Record2;
+    (void)_assert_storage_size_Record2;
 }
 
 
 void SITL::EFI_Hirth::send_record3()
 {
+    // notionally the field updates should happen in the update()
+    // method, but here to save CPU for now:
+    auto &r = packed_record3.record;
+    r.excess_temperature_1 = engine.cht1_temperature;  // cht1
+    r.excess_temperature_2 = engine.cht2_temperature;  // cht2
+    r.excess_temperature_3 = 39;  // egt1
+    r.excess_temperature_4 = 41;  // egt2
+
     packed_record3.update_checksum();
 
-    static_assert(sizeof(record3) == 100, "correct number of bytes in record3");
     write_to_autopilot((char*)&packed_record3, sizeof(packed_record3));
+
+    assert_storage_size<Record3, 100> _assert_storage_size_Record3;
+    (void)_assert_storage_size_Record3;
 }
--- libraries/SITL/SIM_EFI_Hirth.h
+++ libraries/SITL/SIM_EFI_Hirth.h
@@ -86,31 +86,118 @@ private:
 
     class PACKED Record1 {
     public:
-        uint8_t unknown1[10];
+        uint8_t reserved1[2];
+        uint16_t save_in_flash;  // "1 = data are saved in flash automatically"
+        uint8_t reserved2[4];
+        uint16_t engine_status;
         uint16_t rpm;
-        uint8_t unknown2[66];
-        uint16_t air_temperature;
-        uint8_t unknown3[4];
+        uint8_t reserved3[12];
+        uint16_t number_of_interfering_pulses;
+        uint16_t reserved4[2];
+        uint16_t number_of_speed_errors;
+        uint16_t injection_time;
+        uint16_t ignition_angle;
+        uint16_t reserved5;
+        uint16_t voltage_throttle;
+        uint16_t reserved6;
+        uint8_t reserved7[2];
+        uint16_t voltage_engine_temperature;
+        uint16_t voltage_air_temperature;
+        uint8_t reserved8[2];
+        uint16_t voltage_int_air_pressure;
+        uint8_t reserved9[20];
+        int16_t throttle;
+        int16_t engine_temperature;
+        int16_t battery_temperature;
+        int16_t air_temperature;
+        int16_t reserved10;
+        uint16_t sensor_ok;
 
         void update();
     };
 
     class PACKED Record2 {
     public:
-        uint8_t unknown1[62];
-        uint16_t throttle;  // percent * 10
-        uint8_t unknown2[34];
+        uint8_t reserved1[12];
+        int16_t injection_rate_from_basic_graphic_map;
+        int16_t reserved2;
+        int16_t basic_injection_map;
+        int16_t injection_rate_from_air_correction;
+        int16_t reserved3;
+        int16_t injection_rate_from_warming_up_characteristic_curve;
+        int16_t injection_rate_from_acceleration_enrichment;
+        int16_t turn_on_time_of_intake_valves;
+        int16_t injection_rate_from_race_switch;
+        int16_t reserved4;
+        int16_t injection_angle_from_ignition_angle_map;
+        int16_t injection_angle_from_air_temperature_characteristic_curve;
+        int16_t injection_angle_from_air_pressure_characteristic_curve;
+        int16_t ignition_angle_from_engine_temperature_characteristic_curve;
+        int16_t ignition_angle_from_acceleration;
+        int16_t ignition_angle_from_race_switch;
+        uint32_t total_time_in_26ms;
+        uint32_t total_number_of_rotations;
+        uint16_t fuel_consumption;
+        uint16_t number_of_errors_in_error_memory;
+        int16_t voltage_input1_throttle_target;
+        int16_t reserved5;
+        int16_t position_throttle_target;
+        uint16_t throttle_percent_times_10;  // percent * 10
+        int16_t reserved6[3];
+        uint16_t time_of_injector_opening_percent_times_10;
+        uint8_t reserved7[10];
+        uint32_t no_of_logged_data;
+        uint8_t reserved8[12];
     };
 
     class PACKED Record3 {
     public:
-        uint8_t unknown1[100];
+        uint16_t voltage_excess_temperature_1;
+        uint16_t voltage_excess_temperature_2;
+        uint16_t voltage_excess_temperature_3;
+        uint16_t voltage_excess_temperature_4;
+        uint16_t voltage_excess_temperature_5;
+        uint8_t reserved1[6];
+        uint16_t excess_temperature_1;  // cht1
+        uint16_t excess_temperature_2;  // cht2
+        uint16_t excess_temperature_3;  // egt1
+        uint16_t excess_temperature_4;  // egt2
+        uint16_t excess_temperature_5;
+        uint8_t reserved2[6];
+        uint16_t enrichment_excess_temperature_cylinder_1;
+        uint16_t enrichment_excess_temperature_cylinder_2;
+        uint16_t enrichment_excess_temperature_cylinder_3;
+        uint16_t enrichment_excess_temperature_cylinder_4;
+        uint8_t reserved3[6];
+        uint16_t enrichment_excess_temperature_bitfield;
+        uint16_t mixing_ratio_oil_pump1;
+        uint16_t mixing_ratio_oil_pump2;
+        uint16_t ouput_value_water_pump;
+        uint16_t ouput_value_fuel_pump;
+        uint16_t ouput_value_exhaust_valve;
+        uint16_t ouput_value_air_vane;
+        uint16_t ouput_value_e_throttle;
+        uint16_t number_of_injections_oil_pump_1;
+        uint32_t system_time_in_ms;
+        uint16_t number_of_injections_oil_pump_2;
+        uint16_t target_rpm;
+        uint16_t FPC;
+        // these appear to be duplicates of the above; one is probably
+        // voltage?
+        uint16_t xenrichment_excess_temperature_cylinder_1;
+        uint16_t xenrichment_excess_temperature_cylinder_2;
+        uint16_t xenrichment_excess_temperature_cylinder_3;
+        uint16_t xenrichment_excess_temperature_cylinder_4;
+        uint16_t voltage_input_temperature_crankshaft_housing;
+        uint16_t temperature_crankshaft_housing;
+        uint8_t reserved4[14];
     };
 
     class PACKED SetValues {
     public:
-        uint16_t throttle;  // percent * 10
-        uint8_t unknown1[18];
+        int16_t throttle;  // percent * 10
+        int16_t rpm;;
+        int8_t reserved1[16];
     };
 
     // these records are just used for initial values of the fields;
@@ -135,6 +222,19 @@ private:
     uint8_t receive_buf_ofs;
 
     float throttle;
+
+    uint16_t engine_status_field_value() const;
+
+    void init();
+    bool init_done = false;
+
+    // engine model:
+    void update_engine_model();
+    struct {
+        float cht1_temperature;  // engine reports in deg-C
+        float cht2_temperature;
+        uint32_t last_update_ms;
+    } engine;
 };
 
 }
