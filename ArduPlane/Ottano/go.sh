e14864b2ef9dee3293128db688e5491b6d350f91
Peter Barker <pbarker@barker.dropbear.id.au>
2023-07-18 21:05:25 +1000

    Ottano: use hirth simulation

--- ArduPlane/Ottano/go.sh
+++ ArduPlane/Ottano/go.sh
@@ -5,4 +5,4 @@
     cp eeprom-base.bin eeprom.bin
 }
 
-nice ../../Tools/autotest/sim_vehicle.py -D -G -f quadplane-ice --console --map -L SpringValley3 --aircraft test "$*" -A --uartF=sim:megasquirt
+nice ../../Tools/autotest/sim_vehicle.py -D -G -f quadplane-ice --console --map -L SpringValley3 --aircraft test "$*" -A --uartF=sim:hirth
